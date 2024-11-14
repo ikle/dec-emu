4f1c6401e572d2186a67a1ae84f4606dd9a23bb3
Alexei A. Smekalkine <ikle@ikle.ru>
2024-11-14 11:02:42 +0300

    rtl/cpu/mips: implement shift operations

--- rtl/cpu/mips-i.v
+++ rtl/cpu/mips-i.v
@@ -44,6 +44,14 @@ module mips_alu (
 	assign ov = F[3:2] == 0 && aov;		/* add/sub overflow	*/
 endmodule
 
+module mips_shift (
+	input [6:0] F, input [31:0] S, T, input [4:0] sa, output [31:0] q
+);
+	wire [4:0] n = F[2] ? S[4:0] : sa;
+
+	assign q = F[1] ? F[0] ? T >>> n : T >> n : T << n;
+endmodule
+
 module mips_ic (
 	input clock, output [31:0] va, next
 );
@@ -75,12 +83,14 @@ endmodule
 
 module mips_id (
 	input clock, input [31:0] op,
-	output reg [6:0] alu_op, output reg imm
+	output reg [4:0] sa, output reg [6:0] alu_op, output reg imm
 );
 	wire [5:0] C  = op[31:26];
+	wire [4:0] SA = op[10:6];
 	wire [5:0] F  = op[5:0];
 
 	always @(posedge clock) begin
+		sa     <= SA;
 		alu_op <= {C[3], C[5] ? 6'b100001 /* addu for VA */ :
 				 C[3] ? {C[2:1] == 1, 2'b0, C[2:0]} : F};
 		imm    <= C[5] | C[3];
@@ -89,29 +99,31 @@ endmodule
 
 module mips_ex (
 	input clock,
-	input [31:0] S, T, I, PC, input [6:0] F, input i,
+	input [31:0] S, T, I, PC, input [4:0] sa, input [6:0] F, input i,
 	output reg [31:0] D
 );
-	wire [31:0] AD;
+	wire [31:0] AD, XD;
 	wire ov;
 
 	mips_alu   A (F, S, i ? I : T, AD, ov);
+	mips_shift X (F, S, T, sa, XD);
 
 	always @(posedge clock)
-		D  <= AD;
+		D  <= F[5] ? AD : XD;
 endmodule
 
 module mips_core (
 	input clock, output [31:0] PC, input [31:0] op
 );
+	wire [4:0] sa;
 	wire [6:0] F;
 	wire [31:0] S, T, I, D, RN, EN;
 	wire i, ov;
 
 	mips_ic IC (clock, PC, RN);
 	mips_rf RF (clock, op, RN, S, T, I, EN);
-	mips_id ID (clock, op, F, i);
-	mips_ex EX (clock, S, T, I, EN, F, i, D);
+	mips_id ID (clock, op, sa, F, i);
+	mips_ex EX (clock, S, T, I, EN, sa, F, i, D);
 endmodule
 
 `endif  /* CPU_MIPS_I_V */
