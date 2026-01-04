6e8cecd4e8fcb6a3d78797072895f49dcd8a442e
Alexei A. Smekalkine <ikle@ikle.ru>
2026-01-04 13:07:31 +0300

    pdp11: separate instruction simulator top code

--- src/pdp-11.c
+++ src/pdp-11.c
@@ -58,55 +58,5 @@ static int pdp_pull (struct pdp *o, int code, int size, int *x, int *D, int *A)
 #define pdp_pull_c(o, code, size, x, D, A) \
 	do { if (!pdp_pull (o, code, size, x, D, A))  return 0; } while (0)
 
-#include "pdp11-op-fc.h"
-#include "pdp11-op-alu.h"
+#include "pdp11-op-top.h"
 
-/* pdp11-id-top */
-
-static inline int pdp_step_z8 (struct pdp *o, int op)
-{
-	const int fn = BITS (op, 3, 4);
-
-	if (op <  64)  return pdp_sys (o, op);
-	if (op < 128)  return pdp_jmp (o, op);
-
-	switch (fn) {
-	case 0:  return pdp_rts (o, op);			/* RTS */
-	case 1:
-	case 2:
-	case 3:  return 0;					/* SPL */
-	case 4:
-	case 5:  return pdp_clcc (&o->PS, op);			/* CLx */
-	case 6:
-	case 7:  return pdp_secc (&o->PS, op);			/* SEx */
-	default: return pdp_swab (o, op);
-	}
-}
-
-static inline int pdp_step_x3 (struct pdp *o, int op, int B)
-{
-	const int fn = BITS (op, 8, 4);
-
-	switch (fn) {
-	default:  return op < 256 ? pdp_step_z8 (o, op) : pdp_bcc (o, op, B);
-	case  8:
-	case  9:  return pdp_srv   (o, op, B);
-	case 10:
-	case 11:  return pdp_sop   (o, op, B);
-	case 12:  return pdp_shift (o, op, B);
-	case 13:
-	case 14:
-	case 15:  return 0;
-	}
-}
-
-int pdp_step (struct pdp *o, int op)
-{
-	const int B = (op < 0), fn = BITS (op, 12, 3);
-
-	switch (fn) {
-	case  0:  return pdp_step_x3 (o, op, B);
-	case  7:  return 0;					/* exts */
-	default:  return pdp_dop (o, op, B);
-	}
-}
