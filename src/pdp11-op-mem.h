158f0456a73928742a2849b121045448c6ea7037
Alexei A. Smekalkine <ikle@ikle.ru>
2026-08-21 01:32:58 +0300

    pdp11-op-mem: remove extra helper, simplify

--- src/pdp11-op-mem.h
+++ src/pdp11-op-mem.h
@@ -43,11 +43,6 @@ static inline int pdp_lda (struct pdp *o, int op, int B)
 	}
 }
 
-static inline int pdp_ld (struct pdp *o, int op, int B, int *x)
-{
-	return pdp_lda (o, op, B) && pdp_read (o, o->A, x);
-}
-
 static inline int pdp_fetch (struct pdp *o, int op, int B, int n, int *x)
 {
 	op = (n == 0 ? op >> 6 : op);
@@ -55,7 +50,7 @@ static inline int pdp_fetch (struct pdp *o, int op, int B, int n, int *x)
 	if ((op & 070) == 0)
 		return o->reg = 1, *x = o->R[op & 7], 1;
 
-	return o->reg = 0, pdp_ld (o, op, B, x);
+	return o->reg = 0, pdp_lda (o, op, B) && pdp_read (o, o->A, x);
 }
 
 static inline int pdp_commit (struct pdp *o, int op, int B, int z)
